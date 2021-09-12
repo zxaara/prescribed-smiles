ccbcca4e6b142bbce10746a7e7509cd4b9a87050
zxaara <90544031+zxaara@users.noreply.github.com>
2021-09-12 05:04:14 -0400

    Create style.js

---
+++ style.js
@@ -0,0 +1,13 @@
+var cardContainer = $('.card-container');
+var submit = $('.submit-btn');
+
+submit.on("click", appendSomeoneJoins);
+
+function appendSomeoneJoins(event) {
+  event.preventDefault();
+  var name = $('.name').val();
+
+  cardContainer.append(`
+    <p class="name-card"> Hooray, ${name} joined the newsletter!🥳🎉</p>
+  `);
+}
